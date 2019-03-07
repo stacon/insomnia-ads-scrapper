608f107b92e5b5a99e24b72337bfd38654e7f3dc
Constantinos Stathis <constantinos-@hotmail.com>
2019-03-07 23:18:30 +0200

    Fixed index.js to listen to request
    - For heroku usage

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const {serverStart} = require('./libs/serverStart');
 
+const http = require('http');
 serverStart();
+
+http.createServer((request, response)=> {
+  console.log(request);
+  response.writeHead(200);
+  response.end();
+}).listen(process.env.PORT || 5000);
