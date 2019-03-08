489e97e8860c29c1ad38e6e6a448ed6247110a36
Constantinos Stathis <constantinos-@hotmail.com>
2019-03-08 16:47:34 +0200

    Common set interval

--- libs/serverStart.js
+++ libs/serverStart.js
@@ -1,5 +1,6 @@
 const scrapeInsomniaAdsFrom = require('./scrapeInsomniaAdsFrom');
 const Log = require('./Log.class');
+const https = require("https");
 const { newDataHandler } = require('./newDataHandler');
 const {
   adUrls,
@@ -13,14 +14,11 @@ const serverStart = () => {
     newDataHandler(c.data.ads, c.data.categoryName, dataScraped, true)));
 
   setInterval(() => {
-    adUrls.forEach((url) => scrapeInsomniaAdsFrom(url).then((c) =>
-      newDataHandler(c.data.ads, c.data.categoryName, dataScraped)));
+    adUrls.forEach((url) => scrapeInsomniaAdsFrom(url).then((c) => {
+      https.get("https://intense-ridge-99767.herokuapp.com");
+      newDataHandler(c.data.ads, c.data.categoryName, dataScraped)
+    }));
   }, adsCheckIntervalInMinutes * 60000);
-
-  const https = require("https");
-  setInterval(() => {
-    https.get("https://intense-ridge-99767.herokuapp.com");
-  }, 300000)
 };
 
 module.exports = { serverStart };
