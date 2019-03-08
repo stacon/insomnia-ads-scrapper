56e168540e713f5b551bc6c60dffb2a6f7fc5de6
Constantinos Stathis <constantinos-@hotmail.com>
2019-03-08 14:58:50 +0200

    Non Sleeping app

--- libs/serverStart.js
+++ libs/serverStart.js
@@ -1,6 +1,6 @@
 const scrapeInsomniaAdsFrom = require('./scrapeInsomniaAdsFrom');
 const Log = require('./Log.class');
-const {newDataHandler} = require('./newDataHandler');
+const { newDataHandler } = require('./newDataHandler');
 const {
   adUrls,
   adsCheckIntervalInMinutes,
@@ -16,6 +16,11 @@ const serverStart = () => {
     adUrls.forEach((url) => scrapeInsomniaAdsFrom(url).then((c) =>
       newDataHandler(c.data.ads, c.data.categoryName, dataScraped)));
   }, adsCheckIntervalInMinutes * 60000);
+
+  const http = require("http");
+  setInterval(() => {
+    http.get("https://intense-ridge-99767.herokuapp.com");
+  }, 300000)
 };
 
-module.exports = {serverStart};
+module.exports = { serverStart };
